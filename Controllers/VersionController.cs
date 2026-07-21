cdd9e0fbb9563c44ca2131d814c5d199728d8aab
兰梦 <1774036749@qq.com>
2026-07-21 17:40:59 +0800

    Initial commit

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CiCdDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Version = "1.0.0",
+                BuildTime = DateTime.Now
+            });
+        }
+    }
+}
