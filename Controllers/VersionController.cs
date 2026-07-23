3fbe78c2e3ef411088d92e49804f076ffef748b3
兰梦 <1774036749@qq.com>
2026-07-23 10:02:43 +0800

    更新版本

--- Controllers/VersionController.cs
+++ Controllers/VersionController.cs
@@ -11,7 +11,7 @@ namespace CiCdDemo.Controllers
         {
             return Ok(new
             {
-                Version = "1.0.0",
+                Version = "1.0.1",
                 BuildTime = DateTime.Now
             });
         }
