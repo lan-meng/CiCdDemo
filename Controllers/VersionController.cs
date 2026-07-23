b7abad88e42d4c5cdd4d23079224dcf90e3eed33
兰梦 <1774036749@qq.com>
2026-07-23 10:47:29 +0800

    测试github推送

--- Controllers/VersionController.cs
+++ Controllers/VersionController.cs
@@ -11,7 +11,7 @@ namespace CiCdDemo.Controllers
         {
             return Ok(new
             {
-                Version = "1.0.1",
+                Version = "1.0.2",
                 BuildTime = DateTime.Now
             });
         }
